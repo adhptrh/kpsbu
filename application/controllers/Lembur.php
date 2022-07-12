fef6a7cba6961e92be1eb4d391aa07f919f32ec9
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-07-13 05:33:30 +0700

    fix laporan lembur

--- application/controllers/Lembur.php
+++ application/controllers/Lembur.php
@@ -111,7 +111,7 @@ class Lembur extends CI_Controller
     {
         $id_pegawai = $this->input->get("id_pegawai") ?? "semua";
         $periode = $this->input->get("periode") ?? date("Y-m");
-        $query = "SELECT * FROM tb_lembur a LEFT JOIN pegawai b ON a.id_pegawai = b.nip WHERE a.id_pegawai = '$id_pegawai' AND a.status = 3";
+        $query = "SELECT * FROM tb_lembur a LEFT JOIN pegawai b ON a.id_pegawai = b.nip WHERE a.id_pegawai = '$id_pegawai' AND tgl_pengajuan LIKE '$periode%' AND a.status = 3";
         if ($id_pegawai == "semua") {
             $query = "SELECT * FROM tb_lembur a LEFT JOIN pegawai b ON a.id_pegawai = b.nip WHERE a.status = 3";
         }
